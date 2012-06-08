fefc24d70c68bd5e5bcc42346d63247a59a21579
Dominique Broeglin <dominique.broeglin@gmail.com>
2012-06-08 10:16:06 +0200

    Using async mode for do_rpc

--- lib/app/models/ooor_client.rb
+++ lib/app/models/ooor_client.rb
@@ -26,7 +26,7 @@ module Ooor
     
     def call2(method, *args)
       request = create().methodCall(method, *args)
-      data = (["<?xml version='1.0' encoding='UTF-8'?>\n"] + do_rpc(request, false).lines.to_a[1..-1]).join  #encoding is not defined by OpenERP and can lead to bug with Ruby 1.9
+      data = (["<?xml version='1.0' encoding='UTF-8'?>\n"] + do_rpc(request, true).lines.to_a[1..-1]).join  #encoding is not defined by OpenERP and can lead to bug with Ruby 1.9
       parser().parseMethodResponse(data)
     rescue RuntimeError => e
       begin
